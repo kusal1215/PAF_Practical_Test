978579162030747cf17488c2a85d7cf2b179338d
kusal1215 <kusalchathuranga2@gmail.com>
2020-05-05 18:54:33 +0530

    develop:add DB folder to repo

---
+++ DatabaseFile/IT18119572 .sql
@@ -0,0 +1,14 @@
+
+create database User;
+use user;
+
+create table user(
+UserNic varchar(15) not null primary key,
+UserName varchar(100),
+UserGender varchar(50),
+UserContact varchar(20),
+UserEmail varchar(50),
+UserPassword varchar(20) 
+)
+
+
